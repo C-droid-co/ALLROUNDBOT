a871d99370d568ba7ca1527bf2cb6b50aca568f0
C-droid-co <103402546+C-droid-co@users.noreply.github.com>
2024-09-29 05:02:51 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# Configuration file
+
+# MongoDB connection string
+MONGODB_URI = "mongodb+srv://<username>:<password>@cluster0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+# Telegram bot token
+BOT_TOKEN = "YOUR_BOT_TOKEN"
